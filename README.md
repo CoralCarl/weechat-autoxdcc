9bd7f5ec11bbd5525dd8861cb0c8419deed7fd65
carl <Tim.Heumer@gmail.com>
2021-02-28 15:44:04 +0100

    completely rewritten to v1.0
    - support for setting resolution
    - restructured commands
    - matching titles looser

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-# weechat-autoxdcc
- WeeChat python auto downloader for subscribed anime shows
+# weechat-autoxdcc	
+ autoXDCC or short axdc is a small script to automatically download anime shows via xdcc.
+
+## setup
+ To install the script either use `</script load $PATH_TO_FILE/autoxdcc.py>` or move the script into the `<weechat/python/autload>` folder. ([WeeChat Documentation](https://weechat.org/files/doc/devel/weechat_scripting.en.html#load_script))
+
+ For initial setup you will have to add a bot to download from and add a channel you want to listen to for announcements. You will still have to join that channel manually for it to work.
+
+ Autoaccept downloads has to be enabled in WeeChat unless you want to accept each file manually.
+ `</set xfer.file.auto_accept_files on>`
+
+## usage
+ To add a show/bot/channel:
+ `</axdc add show/bot/channel title>`
+ By default order and seperation do not matter as long as every word of the title is present. To check for an exact match use quotation marks instead: `<"title">`
+
+To remove a show/bot/channel:
+`</axdc remove show/bot/channel/hash title/index>`
+
+To remove all entries of a list:
+`</axdc clear show/bot/channel>`
+
+To set the video resolution to be downloaded:
+`</axdc set quality 480/540/720/1080>`
+480p and 540p are released mixed at the moment so they probably shouldn't be used.
+
+The autoXDCC buffer will let you use commands without the `</axdc>` prefix.
--- autoxdcc.py
+++ autoxdcc.py
@@ -2,23 +2,42 @@ import weechat
 import re
 import os
 import os.path
+import json
 
-weechat.register("autoxdcc", "ccarl", "0.1", "none",
+VERSION = "1.0"
+
+weechat.register("autoxdcc", "ccarl", VERSION, "none",
                  "xdcc auto download script", "", "")
 
 HOMEDIR = weechat.info_get("weechat_dir", "") + "/autoxdcc/"
-cfg_types = ["bot", "show", "channel", "hash"]
-cfg = dict()
+CONF_PATH = HOMEDIR + "autoxdcc.conf"
+HASH_PATH = HOMEDIR + "completed"
 
-NEWS = weechat.info_get("irc_buffer", "rizon,#news")
+db = {"version": VERSION, "option": {"quality": "1080"},
+      "bot": [], "show": [], "channel": [], "hash": []}
+valid_commands = r"(?i)"\
+    r"(?:(add|remove)\s+(show|bot|channel|hash)\s+(.*\S)\s*)"\
+    r"|(?:(clear)\s+(show|bot|channel|hash)\s*)"\
+    r"|(?:(list)\s+(show|bot|channel|hash|options)\s*)"\
+    r"|(?:(set)\s+(quality)\s+(480|540|720|1080)p?\s*)"
 
 
 def log_input_cb(data, buffer, input_data):
-    weechat.prnt(buffer, weechat.prefix("action") + input_data)
+    handle_commands("", "", input_data)
     return weechat.WEECHAT_RC_OK
 
 
 def log_close_cb(data, buffer):
+    return weechat.WEECHAT_RC_OK
+
+
+def response(msg):
+    """Prints msg to current Buffer
+
+    Returns WEECHAT_RC_OK
+    """
+
+    weechat.prnt(weechat.current_buffer(), msg)
     return weechat.WEECHAT_RC_OK
 
 
@@ -28,96 +47,148 @@ def log(message):
         logbuf = weechat.buffer_new(
             "autoXDCC", "log_input_cb", "", "log_close_cb", "")
         weechat.buffer_set(logbuf, "autoXDCC", "autoXDCC output log")
-    weechat.prnt(logbuf, weechat.prefix("network") + message)
+    weechat.prnt(logbuf, weechat.prefix("network") + str(message))
 
 
-def update_files():
-    for t in cfg_types:
-        with open(HOMEDIR+t, "w") as f:
-            for i in cfg[t]:
-                f.write(i + "\n")
+def upgrade_database():
+    while db["version"] != VERSION:
+        db["version"] = VERSION
+    update_conf()
+
+
+def update_conf():
+    if not os.path.exists(HOMEDIR):
+        os.mkdir(HOMEDIR)
+    with open(CONF_PATH, "w") as f:
+        json.dump({k: db[k] for k in db if k != "hash"}, f)
+
+
+def update_hash():
+    if not os.path.exists(HOMEDIR):
+        os.mkdir(HOMEDIR)
+    with open(HASH_PATH, "w") as f:
+        json.dump(db["hash"], f)
 
 
 def handle_commands(data, buffer, args):
-    cmd = args.split()
-    if len(cmd) > 2 and cmd[1] in cfg_types:
-        if cmd[0] == "add":
-            cfg[cmd[1]].append(" ".join(cmd[2:]).lower())
-            update_files()
-            weechat.prnt(weechat.current_buffer(),
-                         "%s added to %s list." % (" ".join(cmd[2:]), cmd[1]))
-        elif cmd[0] == "del":
-            if " ".join(cmd[2:]) in cfg[cmd[1]]:
-                cfg[cmd[1]].remove(" ".join(cmd[2:]))
-                update_files()
-                weechat.prnt(weechat.current_buffer(), "%s deleted from %s list." % (
-                    " ".join(cmd[2:]), cmd[1]))
-            elif cmd[2] == "-all":
-                cfg[cmd[1]] = []
-                update_files()
-                weechat.prnt(weechat.current_buffer(),
-                             "%s list cleared." % cmd[1])
-    elif len(cmd) == 2 and cmd[0] == "list" and (cmd[1] in cfg_types or cmd[1] == "-all"):
-        if cmd[1] == "-all":
-            weechat.prnt(weechat.current_buffer(),
-                         "%s list: %s" % (cmd[1], str(cfg)))
-        else:
-            weechat.prnt(weechat.current_buffer(),
-                         "%s list: %s" % (cmd[1], str(cfg[cmd[1]])))
+    g = re.fullmatch(valid_commands, args)
+    if not g:
+        return response("Unknown axdc command: %s" % args)
+    spl = [x.lower() for x in g.groups() if x]
+    cmd = spl[0]
+    if len(spl) > 1:
+        key = spl[1]
+    if len(spl) > 2:
+        val = spl[2]
 
+    if cmd == "add":
+        if key == "channel":
+            val = val.strip("#")
+        if val in db[key]:
+            return response("%s is already in %ss." % (val, key))
+        db[key].append(val)
+        response("%s was added to %ss." % (val, key))
+    elif cmd == "remove":
+        if key == "channel":
+            val = val.strip("#")
+        if val.isnumeric():
+            val = int(val)
+            if val >= len(db[key]):
+                return response("Index out of range.")
+            val = db[key].pop(val)
+        else:
+            if val not in db[key]:
+                return response("%s could not be found in %ss." % (val, key))
+            db[key].remove(val)
+        response("%s was removed from %ss." % (val, key))
+    elif cmd == "clear":
+        db[key] = []
+        response("%ss were cleared." % key)
+    elif cmd == "list":
+        if key == "options":
+            return response("Options: %s" % str(db["option"]))
+        if db[key] == []:
+            return response("List %ss is empty." % key)
+        response("List of %ss:" % key)
+        if key == "hash":
+            ls = [x.upper() for x in db[key]]
+        else:
+            ls = db[key]
+        for i, v in enumerate(ls):
+            response("[%d]\t%s" % (i, v))
+        return weechat.WEECHAT_RC_OK
+    elif cmd == "set":
+        db["option"][key] = val
+        response("%s was set to %s" % (key, val))
+
+    if key == "hash":
+        update_hash()
+    else:
+        update_conf()
     return weechat.WEECHAT_RC_OK
 
 
 def parse_message(data, signal, signal_data):
     msg = weechat.info_get_hashtable(
         "irc_message_parse", {"message": signal_data})
-    if msg["nick"].lower() in cfg["bot"] and msg["channel"][1:].lower() in cfg["channel"] and "(1080p)" in msg["text"].lower():
-        hash = re.search(r"\[(\w+?)]\.", msg["text"]).group(1)
-        if hash not in cfg["hash"]:
-            for show in cfg["show"]:
-                if show in msg["text"].lower():
-                    xdccmsg = re.search(r"(?i)/msg.+", msg["text"]).group(0)
-                    weechat.command(weechat.buffer_search("==", "irc.rizon.%s" % msg["channel"]), xdccmsg)
-                    cfg["hash"].append(hash)
-                    update_files()
-                    log("new hit for \"%s\" hash: %s" % (show, hash))
-                    break
-        else:
-            log("already downloaded: %s" % hash)
+    txt = msg["text"].lower()
+    if (msg["nick"].lower() in db["bot"] and
+        msg["channel"].strip("#").lower() in db["channel"] and
+        ("(%sp)" % db["option"]["quality"]) in txt and
+            (hash := re.search(r"\[(\w+?)]\.", msg["text"]).group(1).lower()) not in db["hash"]):
+        for show in db["show"]:
+            if ((show[0] == "\"" and show[-1] == "\"" and show[1:-1] in txt) or
+                    (not (show[0] == "\"" and show[-1] == "\"" and all(word in txt for word in show)))):
+                xdccmsg = re.search(r"(?i)/msg.+send #?\d+",
+                                    msg["text"]).group(0)
+                weechat.command(weechat.buffer_search(
+                    "==", "irc.rizon.%s" % msg["channel"]), xdccmsg)
+                db["hash"].append(hash)
+                update_hash()
+                log("New Episode of %s, hash: %s" % (show, hash.upper()))
+                break
     return weechat.WEECHAT_RC_OK
 
 
 def launch():
-    for t in cfg_types:
-        try:
-            f = open(HOMEDIR + t, "r")
-        except FileNotFoundError:
-            if not os.path.exists(HOMEDIR):
-                os.mkdir(HOMEDIR)
-            with open(HOMEDIR + t, "w") as f:
-                pass
-            f = open(HOMEDIR + t, "r")
+    # load config
+    try:
+        with open(CONF_PATH, "r") as f:
+            db.update(json.load(f))
+    except FileNotFoundError:
+        update_conf()
 
-        cfg[t] = f.read().splitlines()
-        f.close()
+    upgrade_database()
 
-    weechat.hook_command("axdc",
-                         "xdcc auto downloader",
-                         "[add|del|list bot|channel|show [name|-all]]",
-                         "del x -all to prune list",
-                         "channel = #name",
+    # load completed hashes
+    try:
+        with open(HASH_PATH, "r") as f:
+            db["hash"] = json.load(f)
+    except FileNotFoundError:
+        update_hash()
+
+    # command hook
+    weechat.hook_command("axdc", "xdcc auto downloader for subscribed shows",
+                         "[add|remove show|bot|channel|hash name|index] | "
+                         "[clear show|bot|channel|hash] | "
+                         "[list show|bot|channel|hash|options] | "
+                         "[set quality 480|540|720|1080]",
+                         "Will match each word of a title seperately unless the title is quotation marks.\n"
+                         "/axdc add show one piece => *one piece* *one of piece* *piece one* etc...\n"
+                         "/axdc add show \"one piece\" => *one piece*"
+                         " .",
+                         "add|remove show|bot|channel|hash"
+                         " || clear show|bot|channel|hash"
+                         " || list show|bot|channel|hash|options"
+                         " || set quality 480|540|720|1080",
                          "handle_commands",
                          "")
 
+    # message parser hook
     weechat.hook_signal("rizon,irc_in2_privmsg", "parse_message", "")
 
-    remsg = re.compile(r"(?i)/msg.+")  # xdcc message
-    rehash = re.compile(r"\[(\w+?)]\.")  # hash
-    log(str(cfg))
-    log("%sautoXDCC v0.1 successfully loaded." %
-        weechat.color("green"))
-
-    # weechat.command(weechat.buffer_search("==", "irc.rizon.#news"), r"/msg -server rizon Ginpachi-Sensei xdcc send #12105")
+    log("%sautoXDCC v%s successfully loaded." %
+        (weechat.color("green"), VERSION))
 
 
 launch()
